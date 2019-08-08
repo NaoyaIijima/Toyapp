c0ff8437e6e5416cf5d1ac0ea484bea04b7b8bd4
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-08-08 19:04:05 +0000

    first commit

---
+++ src/main/java/com/example/toy_app/ToyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyAppApplication.class, args);
+	}
+
+}
